d82eba917f01bfe18855c8bb84152947379040ba
dylan0150 <dylan.hanner@yahoo.co.uk>
2018-05-11 14:25:12 +0100

    added get method to orm

--- src/ORM.js
+++ src/ORM.js
@@ -24,7 +24,7 @@ ORM.prototype.table = function(table_name, callback) {
 	this.ready_tables[table_name] = false
 	table_name = table_name
 	this.db.query('CREATE TABLE IF NOT EXISTS '+mysql.escapeId(table_name)+' ( id INT AUTO_INCREMENT PRIMARY KEY )', {}, function(error, results, fields, query) {
-		console.log("ORM :: "+query.sql, error ? ' :: failed' : ' :: success')
+		ORM.log("ORM :: "+query.sql, error ? ' :: failed' : ' :: success')
 		self.ready_tables[table_name] = true
 		self.event('ready_table_'+table_name)
 		if ( callback instanceof Function ) { callback.apply(null, arguments) }
@@ -47,7 +47,7 @@ ORM.prototype.column = function(column, type, extra, callback, table_name) {
 			sql += results.length == 0 ? " ADD " : " MODIFY ";
 			sql += "COLUMN "+mysql.escapeId(column)+" "+self.parseType(type)+" "+self.parseExtra(extra)
 			self.db.query(sql, {}, function(error) {
-				console.log("ORM :: "+arguments[3].sql, error?' :: failed':' :: success')
+				ORM.log("ORM :: "+arguments[3].sql, error?' :: failed':' :: success')
 				if ( callback instanceof Function ) { callback.apply(null, arguments) }
 			})
 		}
@@ -61,15 +61,52 @@ ORM.prototype.drop = function(column_name, callback, table_name) {
 	if ( typeof column_name == 'undefined' ) {
 		this.ready_tables[table_name] = false
 		this.db.query("DROP TABLE IF EXISTS "+mysql.escapeId(table_name), {}, function(error) {
-			console.log("ORM :: "+arguments[3].sql, error?' :: failed':' :: success')
+			ORM.log("ORM :: "+arguments[3].sql, error?' :: failed':' :: success')
 			if ( callback instanceof Function ) { callback.apply(null, arguments) }
 		})
 	} else {
 		this.db.query("ALTER TABLE "+mysql.escapeId(table_name)+" DROP COLUMN "+mysql.escapeId(column_name), {}, function(error) {
-			console.log("ORM :: "+arguments[3].sql, error?' :: failed':' :: success')
+			ORM.log("ORM :: "+arguments[3].sql, error?' :: failed':' :: success')
 			if ( callback instanceof Function ) { callback.apply(null, arguments) }
 		})
 	}
+	return this
+}
+ORM.prototype.get = function(params, callback, table_name) {
+	const self = this
+	if ( table_name == undefined ) { table_name = this.table_name }
+	if ( !this.ready_tables[table_name] ) { this.on('ready_table_'+table_name, function(table) { self.get.call(self, params, callback, table_name) }); return self; }
+	let sql = "SELECT * FROM "+mysql.escapeId(table_name)
+	let clause = " WHERE "
+	for ( let key in params ) {
+		if ( typeof params[key] == "string" ) {
+			sql += clause+mysql.escapeId(key)+" = "+mysql.escape(params[key])
+		} else {
+			if ( typeof params[key].like != undefined ) {
+				sql += clause+mysql.escapeId(key)+" LIKE "+mysql.escape(params[key].like)
+			}
+			if ( typeof params[key].in != undefined ) {
+				sql += clause+mysql.escapeId(key)+" IN ("+mysql.escape(params[key].in)+")"
+			}
+			if ( typeof params[key].gt != undefined ) {
+				sql += clause+mysql.escapeId(key)+" > "+mysql.escape(params[key].gt)
+			}
+			if ( typeof params[key].lt != undefined ) {
+				sql += clause+mysql.escapeId(key)+" < "+mysql.escape(params[key].lt)
+			}
+			if ( typeof params[key].gte != undefined ) {
+				sql += clause+mysql.escapeId(key)+" >= "+mysql.escape(params[key].gte)
+			}
+			if ( typeof params[key].lte != undefined ) {
+				sql += clause+mysql.escapeId(key)+" <= "+mysql.escape(params[key].lte)
+			}
+		}
+		clause = " AND "
+	}
+	this.db.query(sql, function(error) {
+		ORM.log("ORM :: "+arguments[3].sql, error?' :: failed':' :: success')
+		if ( callback instanceof Function ) { callback.apply(null, arguments) }
+	})
 	return this
 }
 ORM.prototype.parseType = function(type) {
@@ -122,6 +159,9 @@ ORM.prototype.parseExtra = function(extra) {
 	var string = ""
 	if ( extra && extra.notnull ) { string += " NOT NULL " }
 	return string.trim()
+}
+ORM.log = function() {
+	console.log(arguments)
 }
 
 module.exports = ORM
